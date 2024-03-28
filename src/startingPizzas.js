867a4a2b1a694e2c843505e8465a21325805f17b
NickRoccodev11 <nchls.rcc@gmail.com>
2024-03-28 15:06:06 -0400

    changed starting data

--- src/startingPizzas.js
+++ src/startingPizzas.js
@@ -1,5 +1,5 @@
 const startingPizzas = [
-  { id: 1, name: "the big-boy", ingredients: ["pepperoni", "sausage", "meatball"] },
+  { id: 1, name: "the clogger", ingredients: ["pepperoni", "sausage", "meatball"] },
   { id: 2, name: "sicilian", ingredients: ["olives", "mushrooms", "carmelized onions"] },
   { id: 3, name: "garden of delight", ingredients: ["peppers", "spinach", "feta"] },
   { id: 4, name: "the chicago", ingredients: ["meatball", "peppers", "mushrooms"] }
